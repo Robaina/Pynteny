0083fc37e541867cc9040f2692cff5df3f59bec3
Semidán <semidan.robaina@gmail.com>
2022-06-08 12:08:26 +0100

    update

--- test.sh
+++ test.sh
@@ -13,10 +13,10 @@
 #  --hmm_dir /home/robaina/Databases/hmm_PGAP/ \
 #  --in /home/robaina/Documents/Pynteny/tests/MG1655.fasta \
 #  --outdir /home/robaina/Documents/Pynteny/tests/MG1655_results \
-#  --synteny_struc "TIGR00899.1 0 <TIGR00171.1 0 <TIGR00170.1 1 <TIGR00973.1"
+#  --synteny_struc ">TIGR00899.1 0 <TIGR00171.1 0 <TIGR00170.1 1 <TIGR00973.1"
 
 python synteny_search.py \
  --hmm_dir /home/robaina/Databases/hmm_PGAP/ \
- --in /home/robaina/Documents/MAR_database/marref_prodigal_longlabels.faa \
+ --in /home/robaina/Databases/MAR_database/marref_prodigal_longlabels.faa \
  --outdir /home/robaina/Documents/Pynteny/tests/TRAITS_results \
  --synteny_struc ">TIGR00899.1 0 <TIGR00171.1 0 <TIGR00170.1 1 <TIGR00973.1"
